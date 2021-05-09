f7ba76ba1013dea616f207172806baf3349021ad
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-09 18:07:52 +0300

    init

--- NavigationBarStylingInSwiftUI/ContentView.swift
+++ NavigationBarStylingInSwiftUI/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNavView: Bool = false
+    @State private var showModalView: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                NavigationLink(
+                    destination: PushedNavigationView(),
+                    isActive: $showNavView
+                ) {
+                    Button(action: {
+                        showNavView = true
+                    }, label: {
+                        Text("Show Second View")
+                    })
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color(.label))
+                    )
+                    .foregroundColor(Color(.systemBackground))
+                }.padding()
+                Text("Hello World")
+                    .padding()
+            }
+            .navigationTitle("Nav Bar Apperance")
+            .navigationBarItems(trailing: Button(action: {
+                showModalView = true
+            }, label: {
+                Image(systemName: "questionmark.circle.fill")
+                    .font(.title2)
+            }))
+            .sheet(isPresented: $showModalView, content: {
+                ModalSheetView()
+            })
+        }
     }
 }
 
---
+++ NavigationBarStylingInSwiftUI/PushedNavigationView.swift
@@ -0,0 +1,20 @@
+//
+//  PushedNavigationView.swift
+//  NavigationBarStylingInSwiftUI
+//
+//  Created by Ramill Ibragimov on 09.05.2021.
+//
+
+import SwiftUI
+
+struct PushedNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PushedNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PushedNavigationView()
+    }
+}
