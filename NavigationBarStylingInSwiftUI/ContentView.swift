ac08f30827b0f89db233b27f78b0d9de2200e655
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-09 17:57:00 +0300

    Initial Commit

---
+++ NavigationBarStylingInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationBarStylingInSwiftUI
+//
+//  Created by Ramill Ibragimov on 09.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationBarStylingInSwiftUI/NavigationBarStylingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarStylingInSwiftUIApp.swift
+//  NavigationBarStylingInSwiftUI
+//
+//  Created by Ramill Ibragimov on 09.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBarStylingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
